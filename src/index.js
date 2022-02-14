26acd72ff31f7c37e0812179c3f281227f20316e
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-14 11:54:52 -0800

    update UI Logic, append names to TOC
    Co-Authored-By: Jack Jones <jacktjones4@gmail.com>
    Co-authored-by: William Jameson <williamjameson90@gmail.com>
    Co-Authored-by: Jessica Baker <jessb.epicodus@gmail.com>
    Co-authored-by: S-Espinet <s.lou.espinet@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import MonsterList from "./monster-list.js";
 table of contents
 A-E
 Abeloth
-https://www.dnd5eapi/co/api/monsters/${query}
+https://www.dnd5eapi/co/api/monsters/abeleth
 
 const query = $(#name).text();
 
@@ -21,12 +21,46 @@ J-N
 O-Z
 */
 
+
+function displayAThruE(arr) {
+  for (let i = 0; i <= 99; i++) {
+    $("#flipbook").append(`<div id="page">${arr.results[i].name}</div>`);
+  }
+}
+function displayFThruI(arr){
+  for (let i = 100; i <= 175; i++) {
+    $("#flipbook").append(`<div id="page">${arr.results[i].name}</div>`);
+  }
+}
+function displayJThruM(arr){
+  for(let i = 176; i <= 202; i++) {
+    $("#flipbook").append(`<div id="page">${arr.results[i].name}</div>`);
+  }
+}
+function displayNThruU(arr){
+  for(let i = 203; i <= 286; i++){
+    $("#flipbook").append(`<div id="page">${arr.results[i].name}</div>`);
+  }
+}
+function displayVThruZ(arr){
+  for(let i = 287; i <= 331; i++){
+    $("#flipbook").append(`<div id="page">${arr.results[i].name}</div>`);
+  }
+}
 $(document).ready(function () {
   MonsterList.getMonsterList().then(function (monsterListResponse) {
+    console.log(monsterListResponse);
     if (monsterListResponse instanceof Error) {
       throw Error("Sorry there was an error");
     }
     const monsterList = monsterListResponse;
     console.log(monsterList);
+    console.log(monsterList.results[0].name);
+    displayAThruE(monsterList);
+    displayFThruI(monsterList);
+    displayJThruM(monsterList);
+    displayNThruU(monsterList);
+    displayVThruZ(monsterList);
+    
   });
 });
