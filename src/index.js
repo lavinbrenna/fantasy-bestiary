93b8cd6680c3fd8c4f639ad1847a8dbf4fdf7896
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-14 16:30:43 -0800

    update UI functionality
    Co-Authored-By: Jack Jones <jacktjones4@gmail.com>
    Co-Authored-by: William Jameson <williamjameson90@gmail.com>
    Co-Authored-by: Jessica Baker <jessb.epicodus@gmail.com>
    Co-Authored-by: S-Espinet <s.lou.espinet@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,72 +6,73 @@ import "./css/styles.css";
 //import MonsterService from './monster.js';
 import MonsterList from "./monster-list.js";
 
-/*
-table of contents
-A-E
-Abeloth
-https://www.dnd5eapi/co/api/monsters/abeleth
-
-const query = $(#name).text();
-
-Air Elemental
- 
-F-I
-J-N
-O-Z
-*/
-
+// function 
+// let monsterName = `${MonsterList.arr.results[i].name}`;
+// $(`p#${monsterName}`).on('click', function() {
+//   MonsterService.getMonster(monsterName)
 
 function displayAThruZ(arr) {
-  for (let i = 0; i <= 99; i++) {
-    $("#aThruE").append(`<div id="page">${arr.results[i].name}</div>`);
+  for(let i = 0; i <= 99; i++) {
+    $("#aThruE").append(`<p class="beastName">${arr.results[i].name}</p>`);
   }
   for (let i = 100; i <= 175; i++) {
-    $("#fThruI").append(`<div id="page">${arr.results[i].name}</div>`);
+    $("#fThruI").append(`<p class="beastName" id="${arr.results[i].name}">${arr.results[i].name}</p>`);
   }
   for(let i = 176; i <= 202; i++) {
-    $("#jThruM").append(`<div id="page">${arr.results[i].name}</div>`);
+    $("#jThruM").append(`<p class="beastName" id="${arr.results[i].name}">${arr.results[i].name}</p>`);
   }
   for(let i = 203; i <= 286; i++){
-    $("#nThruU").append(`<div id="page">${arr.results[i].name}</div>`);
+    $("#nThruU").append(`<p class="beastName" id="${arr.results[i].name}">${arr.results[i].name}</p>`);
   }
   for(let i = 287; i <= 331; i++){
-    $("#vThruZ").append(`<div id="page">${arr.results[i].name}</div>`);
+    $("#vThruZ").append(`<p class="beastName" id="${arr.results[i].name}">${arr.results[i].name}</p>`);
   }
 }
 
+function appendId(arr){
+  for(let i=0; i <=99; i++){
+    console.log(arr.results[i].index);
+    $('p.beastName').attr('id', `${arr.results[i].index}`);
+  }
+}
+
+
 function attachListeners(){
-  $('.aThruE').on('click', function(){
+  $('h2.aThruE').on('click', function(){
     $("#aThruE").toggle();
     $("#fThruI, #jThruM, #nThruU, #vThruZ").hide();
   });
-  $('.fThruI').on('click', function(){
+  $('h2.fThruI').on('click', function(){
     $("#fThruI").toggle();
     $("#aThruE, #jThruM, #nThruU, #vThruZ").hide();
   });
-  $('.jThruM').on('click', function(){
+  $('h2.jThruM').on('click', function(){
     $('#jThruM').toggle();
     $("#fThruI, #aThruE, #nThruU, #vThruZ").hide();
   });
-  $('.nThruU').on('click', function(){
+  $('h2.nThruU').on('click', function(){
     $('#nThruU').toggle();
     $("#fThruI, #jThruM, #aThruE, #vThruZ").hide();
   });
-  $('.vThruZ').on('click', function(){
+  $('h2.vThruZ').on('click', function(){
     $('#vThruZ').toggle();
     $("#fThruI, #jThruM, #nThruU, #aThruE").hide();
+  });
+  $('p.beastName').on('click', 'id', function(){
+    console.log($(this).text());
   });
 }
 
 $(document).ready(function () {
-  attachListeners();
+  
   $("#fThruI, #jThruM, #nThruU, #aThruE, #vThruZ").hide();
   MonsterList.getMonsterList().then(function (monsterListResponse) {
-    console.log(monsterListResponse);
     if (monsterListResponse instanceof Error) {
       throw Error("Sorry there was an error");
     }
     const monsterList = monsterListResponse;
     displayAThruZ(monsterList);
+    appendId(monsterList);
   });
+  attachListeners();
 });
