cbc6251b776b1f725e1d9438de0f28a5fffb6c19
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-16 17:38:09 -0800

    update page title, update image size on monster pages Co-Authored-By: Jack Jones <jacktjones4@gmail.com> Co-Authored-by: William Jameson <williamjameson90@gmail.com> Co-Authored-by: Jessica Baker <jessb.epicodus@gmail.com> Co-Authored-by: S-Espinet <s.lou.espinet@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Fantasy Bestiary',
+      title: 'Monstronomicon',
       template: './src/index.html',
       inject: 'body'
     })
